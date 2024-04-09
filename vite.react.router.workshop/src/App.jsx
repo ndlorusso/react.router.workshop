56a11cf26a1d87dc59c792be2af06c68a47a6f11
Nick Lorusso <ndlorusso@gmail.com>
2024-04-09 11:34:31 -0500

    nav bar + initial CSS styling

--- vite.react.router.workshop/src/App.jsx
+++ vite.react.router.workshop/src/App.jsx
@@ -1,5 +1,9 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Blue from './Components/Blue';
+import Red from './Components/Red';
+import Home from './Components/Home';
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,9 +11,18 @@ function App() {
   return (
     <>
      <div id="container">
-      <h1>Hello React Router!</h1>
-      <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <div id="navbar">
+        <Link to = "/">Home</Link>
+        <Link to = "/blue">Blue</Link>
+        <Link to = "/red">Red</Link>
+        </div>
+      <div id="main-section">
+        <Routes>
+          <Route path = "/" element = { <Home/> }/>
+          <Route path = "/blue" element = { <Blue/> } />
+          <Route path = "/red" element = { <Red/> } />
+        </Routes>
+        </div>
     </div>
     </>
   )
